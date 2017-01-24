c0390241c55089f760a025c87e0da65f75e776bf
Tim De Pauw <timdp@users.noreply.github.com>
2017-01-24 14:27:08 +0100

    Introduce typed errors

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import EventEmitter from 'eventemitter3'
 import fetch from 'isomorphic-fetch'
 import parse from 'iab-vast-parser'
 import { Wrapper } from 'iab-vast-model'
+import VASTLoaderError from './error'
 import atob from './atob'
 
 const RE_DATA_URI = /^data:.*?(;\s*base64)?,(.*)/
@@ -9,6 +10,8 @@ const DEFAULT_OPTIONS = {
   maxDepth: 10,
   credentials: 'omit'
 }
+
+export { VASTLoaderError }
 
 export default class Loader extends EventEmitter {
   constructor (uri, options, parent) {
@@ -49,13 +52,15 @@ export default class Loader extends EventEmitter {
         this._emit('willParse', { uri, body })
         const vast = parse(body)
         this._emit('didParse', { uri, body, vast })
-        if (vast.ads.length === 0) {
-          throw new Error('No ads found')
+        if (vast.ads.length > 0) {
+          const ad = vast.ads.get(0)
+          if (ad instanceof Wrapper || ad.$type === 'Wrapper') {
+            return this._loadWrapped(ad.vastAdTagURI, vast)
+          }
+        } else if (this._depth > 1) {
+          throw new VASTLoaderError(303)
         }
-        const ad = vast.ads.get(0)
-        return (ad instanceof Wrapper || ad.$type === 'Wrapper')
-          ? this._loadWrapped(ad.vastAdTagURI, vast)
-          : [vast]
+        return [vast]
       })
   }
 
@@ -64,7 +69,7 @@ export default class Loader extends EventEmitter {
       .then(() => {
         const { maxDepth } = this._options
         if (maxDepth > 0 && this._depth + 1 >= maxDepth) {
-          throw new Error(`Maximum VAST chain length of ${maxDepth} reached`)
+          throw new VASTLoaderError(302)
         }
         const childLoader = new Loader(vastAdTagURI, null, this)
         return childLoader.load()
@@ -78,7 +83,8 @@ export default class Loader extends EventEmitter {
     return fetch(this._uri, this._fetchOptions)
       .then((response) => {
         if (!response.ok) {
-          throw new Error(`HTTP ${response.status} ${response.statusText}`)
+          // TODO Convert response to HTTPError
+          throw new VASTLoaderError(900, response)
         }
         return response.text()
       })
